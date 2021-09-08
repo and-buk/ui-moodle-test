df4e06f19c2a54d208f4dea24da4068775b7b205
and-buk <and.bukalov@gmail.com>
2021-09-08 23:01:36 +0300

    Add docstrings, type annotations

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,19 +1,23 @@
 from selenium.webdriver.remote.webelement import WebElement
 
-from models.auth import AuthData
 from locators.login_page_locators import LoginPageLocators
+from models.auth import AuthData
 from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    def is_auth(self):
+    """Класс объекта Page Object с атрибутами и методами, необходимыми для аутентификации пользователя."""
+
+    def is_auth(self) -> bool:
+        """."""
         self.find_element(LoginPageLocators.FORM)
         element = self.find_elements(LoginPageLocators.USER_BUTTON)
         if len(element) > 0:
             return True
         return False
 
-    def is_exit_confirm_button(self):
+    def is_exit_confirm_button(self) -> bool:
+        """Проверяем наличие кнопки подтверждения выхода из учётной записи пользователя."""
         self.find_element(LoginPageLocators.FORM)
         element = self.find_elements(LoginPageLocators.EXIT_CONFIRM)
         if len(element) > 0:
@@ -21,24 +25,31 @@ class LoginPage(BasePage):
         return False
 
     def email_input(self) -> WebElement:
+        """Находим поле для ввода email."""
         return self.find_element(LoginPageLocators.LOGIN)
 
     def password_input(self) -> WebElement:
+        """Находим поле для ввода пароля."""
         return self.find_element(LoginPageLocators.PASSWORD)
 
     def submit_button(self) -> WebElement:
+        """."""
         return self.find_element(LoginPageLocators.SUBMIT)
 
     def user_menu(self) -> WebElement:
+        """."""
         return self.find_element(LoginPageLocators.USER_MENU)
 
     def exit(self) -> WebElement:
+        """Находим кнопку выхода из учётной записи пользователя."""
         return self.find_element(LoginPageLocators.EXIT)
 
     def exit_confirm(self) -> WebElement:
+        """Находим кнопку подтверждения выхода из учётной записи пользователя."""
         return self.find_element(LoginPageLocators.EXIT_CONFIRM)
 
-    def auth(self, data: AuthData):
+    def auth(self, data: AuthData) -> None:
+        """Реализуем алгоритм аутентификации, завершения сеанса работы аутентифицированного пользователя."""
         if self.is_exit_confirm_button():
             self.click_element(self.exit_confirm())
         elif self.is_auth():
@@ -49,4 +60,5 @@ class LoginPage(BasePage):
         self.click_element(self.submit_button())
 
     def auth_login_error(self) -> str:
+        """Находим на веб-странице сообщение об ошибке аутентификации."""
         return self.find_element(LoginPageLocators.LOGIN_ERROR).text
