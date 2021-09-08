347da398759119b8bd38019cd04bdaaa4017b6b3
and-buk <and.bukalov@gmail.com>
2021-09-08 22:56:31 +0300

    Add docstrings, type annotations

--- pages/application.py
+++ pages/application.py
@@ -1,17 +1,27 @@
+from typing import Any
+
+from pages.account_page import AccountPage
 from pages.login_page import LoginPage
 
 
 class Application:
-    def __init__(self, driver, url):
+    """Класс для представления объекта, позволяющего с помощью определённого драйвера взаимодействовать с браузером."""
+
+    def __init__(self, driver: Any, url: str) -> None:
+        """Конструктор объекта."""
         self.driver = driver
         self.url = url
         self.login = LoginPage(self)
+        self.user_data = AccountPage(self)
 
-    def open_main_page(self):
+    def open_main_page(self) -> None:
+        """Открываем в браузере веб-страницу по заданному адресу."""
         self.driver.get(self.url)
 
-    def quit(self):
+    def quit(self) -> None:
+        """Закрываем браузер."""
         self.driver.quit()
 
-    def open_auth_page(self):
+    def open_auth_page(self) -> None:
+        """Открываем в браузере веб-страницу аутентификации пользователя."""
         self.driver.get(self.url + "/login/index.php")
