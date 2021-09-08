793dd50a03315b7a6968d896484e0fffbb3e4d0f
and-buk <and.bukalov@gmail.com>
2021-09-08 22:58:22 +0300

    Add docstrings, type annotations, logger

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -6,8 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from models.auth import AuthData
 from pages.application import Application
 
+logger = logging.getLogger("moodle")
+
 
 def pytest_addoption(parser):
+    """Добавляем опции командной строки."""
     parser.addoption(
         "--base-url",
         action="store",
@@ -23,7 +28,8 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture
-def auth(app, request):
+def auth(app, request) -> None:
+    """Аутентификация пользователя."""
     user = request.config.getoption("--username")
     password = request.config.getoption("--password")
     app.open_auth_page()
@@ -33,9 +39,12 @@ def auth(app, request):
 
 @pytest.fixture(scope="session")
 def app(request):
+    """Адаптер для работы методов объектов PageObject с драйвером браузера."""
+    # Инициализируем драйвер для работы с браузером
     base_url = request.config.getoption("--base-url")
+    logger.info(f"Start moodle {base_url}")
     chrome_options = Options()
-    chrome_options.headless = True
+    chrome_options.headless = False
     fixture = Application(
         webdriver.Chrome(
             ChromeDriverManager().install(), chrome_options=chrome_options
@@ -43,4 +52,26 @@ def app(request):
         base_url,
     )
     yield fixture
+    # Завершаем работу. Закрываем браузер.
     fixture.quit()
+
+
+# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     rep = outcome.get_result()
+#     if rep.when == "call" and rep.failed:
+#         try:
+#             if "app" in item.fixturenames:
+#                 web_driver = item.funcargs["app"]
+#             else:
+#                 logger.error("Fail to take screen-shot")
+#                 return
+#             logger.info('Screen-shot done')
+#             allure.attach(
+#                 web_driver.driver.get_screenshot_as_png(),
+#                 name="screenshot",
+#                 attachment_type=allure.attachment_type.PNG,
+#             )
+#         except Exception as e:
+#             logger.error("Fail to take screen-shot: {}".format(e))
