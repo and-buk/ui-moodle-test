1e0376e4196efaae839ab201a33b87cae5917030
and-buk <and.bukalov@gmail.com>
2021-09-08 22:56:46 +0300

    Add docstrings, type annotations

--- models/auth.py
+++ models/auth.py
@@ -1,15 +1,23 @@
+from __future__ import annotations
+
+from typing import Any
+
 from faker import Faker
 
 fake = Faker("Ru-ru")
 
 
 class AuthData:
-    def __init__(self, login=None, password=None):
+    """Класс для представления объекта с данными, необходимыми для аутентификации пользователя."""
+
+    def __init__(self, login: Any = None, password: Any = None):
+        """Конструктор объекта."""
         self.login = login
         self.password = password
 
     @staticmethod
-    def random():
+    def random() -> AuthData:
+        """Генерируем случайные данные для аутентификации пользователя."""
         login = fake.email()
         password = fake.password()
         return AuthData(login, password)
