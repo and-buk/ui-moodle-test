90aa116a2ebb5ff180c8ffc4a431b9a1af884ac3
and-buk <and.bukalov@gmail.com>
2021-09-08 22:57:46 +0300

    Add docstrings

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -2,5 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
+    """
+    Класс для работы со значениями базовых элементов веб-страниц,
+    изменение которых не предполагается во время тестирования.
+    """
+
     LOG_IN_TEXT = (By.LINK_TEXT, "Вход")
     HEADER_USER_INFO = (By.XPATH, './/span[@class = "usertext mr-1"]')
